6dad6ddcd7805ae93a465a72e01f919e85f8210d
omar guzman <omarjo_90@hotmail.com>
2011-09-29 03:55:11 -0600

    first commit

---
+++ MvcMovie2/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie2.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            //ViewBag.Message = "Welcome to ASP.NET MVC!";
+            ViewBag.Message = "Hello, welcome to my first hello world and the Movie Store";
+
+            return View();
+        }
+
+        /*public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }*/
+
+        public ActionResult About()
+        {
+            return View();
+        }
+    }
+}
---
+++ MvcMovie2/Models/MovieInitializer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace MvcMovie2.Models
+{
+    public class MovieInitializer : DropCreateDatabaseIfModelChanges<MovieDBContext>
+    {
+       protected override void Seed(MovieDBContext context)
+        {
+            var movies = new List<Movie>
+            {
+                new Movie {
+                    Title = "Scary movie",
+                    ReleaseDate = DateTime.Parse("2005-12-12"),
+                    Genre = "comedy",
+                    Rating = "R",
+                    Price = 6500},
+
+                    new Movie {
+                        Title = "El Aro",
+                        ReleaseDate = DateTime.Parse("2012-10-10"),
+                        Genre = "Miedo",
+                        Rating = "R",
+                        Price = 4545},
+
+                        new Movie {
+                        Title = "death race",
+                        ReleaseDate = DateTime.Parse("2006-09-09"),
+                        Genre = "action",
+                        Rating = "R",
+                        Price = 5656},
+
+                        new Movie {
+                        Title = "taken",
+                        ReleaseDate = DateTime.Parse("2007-08-08"),
+                        Genre = "fiction",
+                        Rating = "R",
+                        Price = 7878}
+                    };
+                        
+                    movies.ForEach(d => context.Movies.Add(d));
+                }
+            }
+
+        }
+    
